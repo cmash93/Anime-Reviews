9321e5f25adf3c7e021e9111de4c362edc56c1f2
cmash93 <cmash93@gmail.com>
2023-01-22 14:16:11 -0600

    fixed a few small issues

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const Anime = require('./Anime');
 // const Comment = require('./Comment');
 const User = require('./User');
 
-module.exports = { Anime, Comment, User };
+module.exports = { Anime, User };
