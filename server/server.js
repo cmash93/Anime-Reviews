cbc743f2ae0cd46432ed3f79d37d6c84f3033f60
cmash93 <cmash93@gmail.com>
2023-01-24 10:41:09 -0600

    ?

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(_dirname, '../client/build')));
+}
+
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
     server.applyMiddleware({ app })
