a7a673b9ed3d8b2295c366733b92ee4c4a52bcc7
cmash93 <cmash93@gmail.com>
2023-01-24 10:44:31 -0600

    missing _

--- server/server.js
+++ server/server.js
@@ -9,18 +9,23 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({ 
-    typeDefs, 
+const server = new ApolloServer({
+    typeDefs,
     resolvers,
     context: authMiddleware
 });
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(_dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
 }
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
